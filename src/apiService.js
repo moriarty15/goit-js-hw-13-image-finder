ac8ef91d164306a196d5f5050d146689c10085cd
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-09-18 22:30:01 +0800

    добавил стилизация
    1) поставил фон
    2) изменил размер инпута и размер шрифта ввода и плейсхолдера
    3) для бордера инпута задал радиус
    4) сдал дз поэтому сделал per_page 15(было 12)

--- src/apiService.js
+++ src/apiService.js
@@ -7,7 +7,7 @@ export default class NewApiRequest {
    async feachArticles() {
       const BASE_URL = 'https://pixabay.com/api/';
       const MY_KEY = '23320531-e67f94e9f6229e6b46894ace7'
-      const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${MY_KEY}`;
+      const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=15&key=${MY_KEY}`;
 
       try {const responce = await fetch(url);
       const hits = await responce.json();
